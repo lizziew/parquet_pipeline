0c2d52f957e58ef3b0558d230897de79a05dda34
lizziew <elizabeth.m.wei@gmail.com>
2018-10-30 17:00:12 -0400

    Made querying parquet / timing easier

--- query_parquet.py
+++ query_parquet.py
@@ -12,17 +12,32 @@ if __name__ == "__main__":
     location_file_name = sys.argv[2]
     location_file = open(location_file_name, "r")
     locations = location_file.read().splitlines()
-    df = sqlContext.read.parquet(locations[2])
+    df = sqlContext.read.parquet(locations[3] + "/*.parquet")
     df.registerTempTable("lineitem")
 
-    # Read in  query
+    # Read in query
     query_file_name = sys.argv[1] 
     query_file = open(query_file_name, "r")
     query = query_file.read()
 
-    # Run query 
-    start = time.time()
-    df = sqlContext.sql(query)
-    end = time.time()
-    print(end - start)
-    print("=================")
+    # Read in number of times to run query 
+    total_time = 0.0
+    num_iterations = int(sys.argv[3]) 
+
+    # Warm up 3 times
+    for _ in range(3):
+        df = sqlContext.sql(query)
+
+    # Run query and time 
+    for _ in range(num_iterations):
+        start = time.time()
+        df = sqlContext.sql(query)
+        end = time.time()
+        total_time += (end-start)
+
+    avg_time = float(total_time) / float(num_iterations)
+    
+    # Record time 
+    f = open('./temp.txt', 'w')
+    f.write(str(end-start) + " seconds after running " + str(num_iterations) + " iterations")
+    f.close() 
